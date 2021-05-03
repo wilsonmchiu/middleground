edfb5b471e47d83cd16d41459c84d4d88f4b1c74
azhang36 <azhang36@ucsc.edu>
2021-05-03 00:27:08 -0700

    fixed wrapping of text in suggested articles`

--- src/client/src/views/Article.vue
+++ src/client/src/views/Article.vue
@@ -1,117 +1,80 @@
+<style>
+  .v-card__text, .v-card__title {
+    word-break: normal; /* maybe !important  */
+  }
+</style>
+
 <template>
   <v-container fluid>
     <v-row>
-    <v-col cols="12" sm="12" md="8" lg="8">
-      <v-card-text>
-        <h1>{{articleData.title}}</h1>
-        <br>
-        <subtitle-1>By {{articleData.author}}</subtitle-1>      <br>
-        <subtitle-1>{{articleData.timestamp}}</subtitle-1>
-      </v-card-text>
-      
-      <v-img 
-        :aspect-ratio="5/1"
-        :src="articleData.image">
-      </v-img>
-      <v-card-text>
-        {{articleData.body}}
-      </v-card-text>
-    </v-col>
+      <v-col cols="12" sm = "12" md="8" lg="8">
+        <v-card-text>
+          <h1>{{articleData.title}}</h1>
+          <br>
+          <subtitle-1>By {{articleData.author}}</subtitle-1>      <br>
+          <subtitle-1>{{articleData.timestamp}}</subtitle-1>
+        </v-card-text>
+        
+        <v-img 
+          :aspect-ratio="5/1"
+          :src="articleData.image">
+        </v-img>
+        <v-card-text>
+          {{articleData.body}}
+        </v-card-text>
+      </v-col>
 
-    <!-- <v-navigation-drawer
-      app
-      floating
-      right
-      clipped
-    > -->
+      <v-col cols="12" sm= "12" md="4" lg="4">
+        <hr class="grey--text" />
+        <h4 class="mb-3 mt-3">What People Think</h4>
+        <v-img 
+          :aspect-ratio="5/1"
+          src="https://canvasjs.com/wp-content/uploads/images/gallery/javascript-charts/overview/javascript-charts-graphs-index-data-label.png"></v-img>
+        <h4 class="mb-3 mt-3">Related</h4>
 
-    <v-col cols="12" sm="12" md="4" lg="4">
-              <hr class="grey--text" />
-              <h4 class="mb-3 mt-3">What People Think</h4>
-              <v-img 
-                :aspect-ratio="5/1"
-                src="https://canvasjs.com/wp-content/uploads/images/gallery/javascript-charts/overview/javascript-charts-graphs-index-data-label.png"></v-img>
-              <h4 class="mb-3 mt-3">Related</h4>
+        <div v-for="card in relatedCards" :key="card" class="mb-5">
+          <v-skeleton-loader
+            class="mx-auto"
+            type="list-item-avatar-three-line"
+            :loading="loading"
+            tile
+            large
+          >
+            <v-card class="card" tile flat>
+              <v-row no-gutters>
+                <v-col class="mx-auto" cols="3" sm="3" md="5" lg="5">
+                  <!-- <v-responsive max-height="100%"> -->
+                  <v-img
+                    class="align-center"
+                    :src="card.image"
+                  >
+                  </v-img>
+                  <!-- </v-responsive> -->
+                </v-col>
+                <v-col>
+                  <div class="ml-2">
+                    <v-card-title
+                      class="pl-2 pt-0 subtitle-1 font-weight-bold"
+                      style="line-height: 1"
+                    >
+                      {{card.title}}
+                    </v-card-title>
 
-              <div v-for="card in relatedCards" :key="card" class="mb-5">
-                <v-skeleton-loader
-                  class="mx-auto"
-                  type="list-item-avatar-three-line"
-                  :loading="loading"
-                  tile
-                  large
-                >
-                  <v-card class="card" tile flat>
-                    <v-row no-gutters>
-                      <v-col class="mx-auto" cols="3" sm="3" md="5" lg="5">
-                        <!-- <v-responsive max-height="100%"> -->
-                        <v-img
-                          class="align-center"
-                          :src="card.image"
-                        >
-                        </v-img>
-                        <!-- </v-responsive> -->
-                      </v-col>
-                      <v-col>
-                        <div class="ml-2">
-                          <v-card-title
-                            class="pl-2 pt-0 subtitle-1 font-weight-bold"
-                            style="line-height: 1"
-                          >
-                            {{card.title}}
-                          </v-card-title>
-
-                          <v-card-subtitle
-                            class="pl-2 pt-2 pb-0"
-                            style="line-height: 1"
-                          >
-                            {{card.author}}<br />
-                            {{card.timestamp}}
-                          </v-card-subtitle>
-                        </div>
-                      </v-col>
-                    </v-row>
-                  </v-card>
-                </v-skeleton-loader>
-              </div>
-            </v-col>
-
-
-
-
-    <!-- <v-col cols="12" sm="12" md="4" lg="4">
-      <v-card-text>
-        <h1>What People Think</h1>
-      </v-card-text>
-      <v-img 
-        :aspect-ratio="5/1"
-        src="https://canvasjs.com/wp-content/uploads/images/gallery/javascript-charts/overview/javascript-charts-graphs-index-data-label.png"></v-img>
-
-      <v-card-text>
-        <h1>Related</h1>
-      </v-card-text>
-
-      <v-card-text>
-        Article 1
-      </v-card-text>
-      <v-img 
-        :aspect-ratio="3/2"
-        src="https://i.insider.com/5c5dd439dde867479d106cc2?width=1000&format=jpeg&auto=webp"></v-img>
-      <v-card-text>
-        Article 2
-      </v-card-text>
-      <v-img 
-        :aspect-ratio="3/2"
-        src="https://cdn.vox-cdn.com/thumbor/1i4ECs3zLWaWf32jmEFAc_U4AyI=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/54014619/shrek.0.jpg"></v-img>
-      <v-card-text>
-        Article 3
-      </v-card-text>
-      <v-img 
-        :aspect-ratio="3/2"
-        src="https://i.insider.com/5c5dd439dde867479d106cc2?width=1000&format=jpeg&auto=webp"></v-img>
-    </v-col> -->
+                    <v-card-subtitle
+                      class="pl-2 pt-2 pb-0"
+                      style="line-height: 1"
+                    >
+                      {{card.author}}<br />
+                      {{card.timestamp}}
+                    </v-card-subtitle>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-skeleton-loader>
+        </div>
+      </v-col>
     </v-row>
-    <!-- </v-navigation-drawer> -->
   </v-container>
 </template>
 
