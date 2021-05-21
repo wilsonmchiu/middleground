e42137f804c251726e656042a0cca2b1d23760ab
Wilson Chiu <wilsonmchiu@gmail.com>
2021-05-21 13:49:13 -0700

    refactor

--- src/server/mgflask/db.py
+++ src/server/mgflask/db.py
@@ -5,6 +5,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import event
 import json
+import os
+
+
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    # enables foreign key constraints for SQLite
+    dbapi_con.execute('pragma foreign_keys=ON')
+
 
 with open('creds.json') as data_file:
     data = json.load(data_file)
@@ -15,53 +22,40 @@ CLOUDSQL_DATABASE = data['GCSQL']['db-name']
 CLOUDSQL_CONNECTION_NAME = data['GCSQL']['connection-name']
 
 LOCAL_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{nam}:{pas}@127.0.0.1:3306/{dbn}').format (
+    'mysql+pymysql://{nam}:{pas}@127.0.0.1:3306/{dbn}').format(
     nam=CLOUDSQL_USER,
     pas=CLOUDSQL_PASSWORD,
     dbn=CLOUDSQL_DATABASE,
 )
 
 LIVE_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{nam}:{pas}@localhost/{dbn}?unix_socket=/cloudsql/{con}').format (
+    'mysql+pymysql://{nam}:{pas}@localhost/{dbn}?unix_socket=/cloudsql/{con}').format(
     nam=CLOUDSQL_USER,
     pas=CLOUDSQL_PASSWORD,
     dbn=CLOUDSQL_DATABASE,
     con=CLOUDSQL_CONNECTION_NAME,
 )
 
-# Use this for to access the Google Cloud SQL db. make sure to use Google Cloud Proxy
-# engine = create_engine(LOCAL_SQLALCHEMY_DATABASE_URI, convert_unicode=True)
+if (os.environ.get('FLASK_ENV') is None):
+    engine = create_engine('sqlite:///mgflask.db', convert_unicode=True)
+    event.listen(engine, 'connect', _fk_pragma_on_connect)
+elif (os.environ.get('FLASK_ENV') == "development"):
+    # Use this for to access the Google Cloud SQL db. make sure to use Google Cloud Proxy
+    engine = create_engine(LOCAL_SQLALCHEMY_DATABASE_URI, convert_unicode=True)
+else:
+    # Use this for deployments
+    engine = create_engine(LIVE_SQLALCHEMY_DATABASE_URI, convert_unicode=True)
 
-# Use this for deployments
-engine = create_engine(LIVE_SQLALCHEMY_DATABASE_URI, convert_unicode=True)
-
-# Use this for local testing. make sure to uncomment lines below 
-# engine = create_engine('sqlite:///mgflask.db', convert_unicode=True)
-# # enables foreign key constraints for SQLite
-# def _fk_pragma_on_connect(dbapi_con, con_record):
-#     dbapi_con.execute('pragma foreign_keys=ON')
-# event.listen(engine, 'connect', _fk_pragma_on_connect)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
-# original sql_mode causes a lot of problems. TRADITIONAL seems to work
-def connect(dbapi_con, connection_record):
-        cur = dbapi_con.cursor()
-        cur.execute("SET SESSION sql_mode='TRADITIONAL'")
-        cur = None
-
-event.listen(engine, 'connect', connect)
-
-
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db():
     import mgflask.models
     Base.metadata.create_all(bind=engine)
     print('Created the database structure')
-
-
-
