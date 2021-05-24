64404e8ab51193da9519c2e53ed420cb7312b8d2
Simon Guo <yguo57@ucsc.edu>
2021-05-24 09:44:17 -0700

    new test_news

--- src/server/mgflask/test_news.py
+++ src/server/mgflask/test_news.py
@@ -1,50 +1,68 @@
+import unittest
 import requests
-from mgflask import news_retrieval
-
-# call test_trieval() to put articles into the database
-# call test_endpoint() to get articles from database (make sure the server is running)
-
-
-def test_retrieval():
-    news_retrieval.get_headlines(
-        q="US", qintitle="US", page_size=80, language='en', page=1, sources = "bbc-news, fox-news, the-wall-street-journal, national-review, the-huffington-post, the-hill, cnn")
-    news_retrieval.get_everything(q="US", qintitle="US", page_size=80, language='en',
-                                  page=1, sources="cnn, bbc-news", from_param="2021-04-20", to="2021-05-03")
+import os
+import subprocess
+import time
 
 
-def test_endpoint():
-    baseUrl = "http://127.0.0.1:5000/news"
+######################## initialize variables ################################################
 
-    response = requests.get(baseUrl, params={'source': ["cnn", 'bbc-news'], 'title': [
+host = "http://127.0.0.1:5000"
+endpoint = "news"
+baseUrl = host + "/"+endpoint
+app_name = 'mgflask'
+server_proc = None
+
+
+################################# Unit Test Class ############################################################
+
+class TestNews(unittest.TestCase):
+
+    def test_1_list_params(self):
+        response = requests.get(baseUrl, params={'source': ["cnn", 'bbc-news'], 'title': [
                             "opinion", "covid"], 'publishedAt': ['2021-04-25', '2021-05-03'], 'limit_articles': 5})
-    print("----------------test1 list parameters----------------")
-    for article in response.json()['articles']:
-        print("source: ", article['source'])
-        print("title: ", article['title'])
-        print("publishedAt: ", article['publishedAt'])
-
-    response = requests.get(baseUrl, params={
-                            'source': "cnn", 'title': "opinion", 'publishedAt': '2021-04-25', 'limit_articles': 5})
-    print("----------------test2 single parameters----------------")
-    for article in response.json()['articles']:
-        print("source: ", article['source'])
-        print("title: ", article['title'])
-        print("publishedAt: ", article['publishedAt'])
-
-    response = requests.get(baseUrl)
-    print("----------------test3 no parameters ----------------")
-    for article in response.json()['articles'][:5]:
-        print("source: ", article['source'])
-        print("title: ", article['title'])
-        print("publishedAt: ", article['publishedAt'])
-
-    response = requests.get(baseUrl, params={'partition_by': 'source', 'limit_articles': 50, 'publishedAt': ['2021-04-20', '2021-05-31']})
-    print("----------------test4 parameter:partition_by ----------------")
-    for partition_key, partition in response.json()['articles'].items():
-        print( partition_key, ": ")
-        for article in partition:
+        print("----------------test1 list parameters----------------")
+        for article in response.json()['articles']:
             print("source: ", article['source'])
             print("title: ", article['title'])
             print("publishedAt: ", article['publishedAt'])
-            
+    
+    def test_2_single_paramself(self):        
+        response = requests.get(baseUrl, params={'source': "cnn", 'title': "opinion", 'publishedAt': '2021-04-25', 'limit_articles': 5})
+        print("----------------test2 single parameters----------------")
+        for article in response.json()['articles']:
+            print("source: ", article['source'])
+            print("title: ", article['title'])
+            print("publishedAt: ", article['publishedAt'])
 
+    def test_3_no_paramself(self):
+        response = requests.get(baseUrl)
+        print("----------------test3 no parameters ----------------")
+        for article in response.json()['articles'][:5]:
+            print("source: ", article['source'])
+            print("title: ", article['title'])
+            print("publishedAt: ", article['publishedAt'])
+
+    def test_4_partition_by(self):
+        response = requests.get(baseUrl, params={'partition_by': 'source', 'limit_articles': 50, 'publishedAt': ['2021-04-20', '2021-05-31']})
+        print("----------------test4 parameter:partition_by ----------------")
+        for partition_key, partition in response.json()['articles'].items():
+            print( partition_key, ": ")
+            for article in partition:
+                print("source: ", article['source'])
+                print("title: ", article['title'])
+                print("publishedAt: ", article['publishedAt'])
+
+'''
+    @classmethod
+    def setUpClass(cls):
+        server_proc = subprocess.Popen("export FLASK_APP="+app_name+ " && flask run", shell = True, cwd="..")
+        time.sleep(3)
+
+    @classmethod
+    def tearDownClass(cls):
+       server_proc.kill()
+       '''
+
+if __name__ == '__main__':
+    unittest.main()
