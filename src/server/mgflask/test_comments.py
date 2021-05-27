b151f1b7377520fa545fa06c1f9d04b7499f2a7c
azhang36 <azhang36@ucsc.edu>
2021-05-27 16:54:37 -0700

    unit testing for post comments

--- src/server/mgflask/test_comments.py
+++ src/server/mgflask/test_comments.py
@@ -1,19 +1,61 @@
 import unittest
 
 from flask import Flask, jsonify, Blueprint
-from mgflask.db import db_session
+from mgflask.db import db_session, Base, engine
 from mgflask.models import User, Article, Comment, Reply, ArticleRating, CommentRating, ReplyRating
 from mgflask import comments
+from comments import bp
+from flask import json
 
 class TestComments(unittest.TestCase):
+    def setUp(self):
+        Base.metadata.drop_all(bind=engine) 
+        user1 = User(username='GOODUSER', password='password')
+        article1 = Article(author='ARTICLE 1', source='t source', title='title', content='content', description='desc')
+        db_session.add(user1)
+        db_session.add(article1)
+    
+    def tearDown(self):
+        Base.metadata.drop_all(bind=engine) 
 
-    def test_insert(self):
-        comment1 = Comment(article_id='1', username='Andrew', content='COMMENT 1')
-        db_session.add(comment1)
-        comments = db_session.query(Comment).all()
-        self.assertIn(comment1, comments)
+    def post_bad_username(self):
+        testdata = json.dumps({'username': "BADUSER", 'articleID': '1', 'userComment': "Comment 1"})
+        response = bp.test_client().post(
+            '/post',
+            data=testdata,
+            content_type='application/json',
+        )
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.msg, "Unsuccessful. Check Exceptions.")
+        self.assertEqual(response.exception, "username BADUSER does not exist in the database")
+        comments = db_session.query(Comment).one()
+        self.assertIsNone(comments)
 
-        reply1 = Reply(comment_id='1', username='user1', content='content')
-        db_session.add(reply1)
-        replies = db_session.query(Reply).all()
-        self.assertIn(reply1, replies)
+    def post_bad_article(self):
+        testdata = json.dumps({'username': "GOODUSER", 'articleID': '5555', 'userComment': "Comment 1"})
+        response = bp.test_client().post(
+            '/post',
+            data=testdata,
+            content_type='application/json',
+        )
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.msg, "Unsuccessful. Check Exceptions.")
+        self.assertEqual(response.exception, "article id 5555 does not exist in the database")
+        comments = db_session.query(Comment).one()
+        self.assertIsNone(comments)
+
+    def post_success(self):
+        testdata = json.dumps({'username': "GOODUSER", 'articleID': '1', 'userComment': "Comment 1"})
+        response = bp.test_client().post(
+            '/post',
+            data=testdata,
+            content_type='application/json',
+        )
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.msg, "comment successful")
+        self.assertEqual(response.insert_status, "success")
+        comments = db_session.query(Comment).one()
+        self.assertIsNotNone(comments)
+    
+if __name__ == '__main__':
+    unittest.main()
