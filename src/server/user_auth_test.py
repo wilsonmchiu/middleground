62d6de379259b559ebe7c91a6bcc429f63758c27
Jessy <jessyarmstrong761@gmail.com>
2021-05-26 22:57:24 -0700

    Register and 1 login

--- src/server/user_auth_test.py
+++ src/server/user_auth_test.py
@@ -1,20 +1,21 @@
 import unittest
-from flask import current_app
-from mgflask.db import Base
+import requests
+from mgflask.db import Base, engine, db_session
 from mgflask.models import User
-from mgflask.auth import register,login
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-test_engine = create_engine('sqlite:///test.db')
-test_db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=test_engine))
-
-Base.query = test_db_session.query_property()
-Base.metadata.create_all(bind=test_engine)
+from mgflask.auth import (
+    test_users,
+    get_user,
+)
 
 print('Created the test database structure')
+
+host = "http://127.0.0.1:5000"
+endpoint = "auth"
+BASE_URL = host + "/"+endpoint
+REGISTER_URL = BASE_URL + "/register"
+LOGIN_URL = BASE_URL + "/login"
+
+
 
 class TestUser(unittest.TestCase):
 
@@ -24,6 +25,7 @@ class TestUser(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        Base.metadata.drop_all(bind=engine)
         print('teardownClass')
 
     def setUp(self):
@@ -36,24 +38,57 @@ class TestUser(unittest.TestCase):
             "username": "Clark Kent",
             "password": "Superman!2@#"
         }
-
+    
     def testRegisterEmptyDB(self):
         """
-        Tests registering a user and upon retrieval should have the same username
+        Tests registering a user and upon 
+        retrieval should have the same username
         """
-        return
+        x = requests.post(REGISTER_URL, json = self.post_data1)
+        user_expected = db_session.query(User).filter_by(username=self.post_data1["username"]).first()
 
-
-    def testRegisterSameUserDB(self):
-        """
-        Tests that a user register on a username that already exists 
-        properly throws an error
-        """
-        return
+        self.assertEqual(x.status_code, 201)
+        self.assertIsNotNone(user_expected)
+        self.assertEqual("Barry Allen", user_expected.username)
         
+    def testRegisterSameUser(self):
+        """
+        Tests registering a user that is already registered
+        """
+        register_1 = requests.post(REGISTER_URL, json = self.post_data2)
+        user_expected = db_session.query(User).filter_by(username=self.post_data2["username"]).first()
 
+        self.assertEqual(register_1.status_code, 201)
+        self.assertIsNotNone(user_expected)
+        register_2 = requests.post(REGISTER_URL, json = self.post_data2)
+        self.assertEqual(register_2.status_code, 401)
+        self.assertEqual(register_2.json()["msg"],
+                            "User already registered")
+    
+    def testRegisterNoUsername(self):
+        """
+        Tests registering a user with 
+        improper parameter of no username
+        """
+        corrupt_data = {"username": "", "password": "passwoor123"}
+        corrupt_post = requests.post(REGISTER_URL, json = corrupt_data)
+        self.assertEqual(corrupt_post.status_code, 401)
+        self.assertEqual(corrupt_post.json()["msg"], "Username is required")
 
-
+    def testRegisterNoPassword(self):
+        """
+        Tests registering a user with the improper parameters 
+        of an empy password
+        """
+        corrupt_data = {"username": "noPassword", "password": ""}
+        corrupt_post = requests.post(REGISTER_URL, json = corrupt_data)
+        self.assertEqual(corrupt_post.status_code, 401)
+        self.assertEqual(corrupt_post.json()["msg"], "Password is required.")
+    
+    def testNoUserLogin(self):
+        post_data = requests.post(LOGIN_URL, json=self.post_data1)
+        self.assertEqual(post_data.status_code, 401)
+        self.assertEqual(post_data.json()["msg"], "Incorrect username.")
 
 
 
