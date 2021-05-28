83bd4ae3b28cf7d1ca5f56f2ac036c94871b9ff7
Jessy <jessyarmstrong761@gmail.com>
2021-05-28 12:49:10 -0700

    testing independent of local server

--- src/server/user_auth_test.py
+++ src/server/user_auth_test.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+from mgflask import app, db
 from mgflask.db import Base, engine, db_session
 from mgflask.models import User
 from mgflask.auth import (
@@ -8,14 +9,16 @@ from mgflask.auth import (
 )
 
 host = "http://127.0.0.1:5000"
-endpoint = "auth"
-BASE_URL = host + "/"+endpoint
+endpoint = "/auth"
+BASE_URL = host + endpoint
 REGISTER_URL = BASE_URL + "/register"
 LOGIN_URL = BASE_URL + "/login"
+app.testing = True
+client = app.test_client()
 
 
+class AuthTest(unittest.TestCase):
 
-class Test(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
@@ -40,9 +43,9 @@ class Test(unittest.TestCase):
         Tests registering a user and upon 
         retrieval should have the same username
         """
-        x = requests.post(REGISTER_URL, json = self.post_data1)
-        user_expected = db_session.query(User).filter_by(username=self.post_data1["username"]).first()
+        x = client.post(REGISTER_URL, json=self.post_data1)
 
+        user_expected = db_session.query(User).filter_by(username=self.post_data1["username"]).first()
         self.assertEqual(x.status_code, 201)
         self.assertIsNotNone(user_expected)
         self.assertEqual("Barry Allen", user_expected.username)
@@ -51,14 +54,14 @@ class Test(unittest.TestCase):
         """
         Tests registering a user that is already registered
         """
-        register_1 = requests.post(REGISTER_URL, json = self.post_data2)
+        register_1 = client.post(REGISTER_URL, json = self.post_data2)
         user_expected = db_session.query(User).filter_by(username=self.post_data2["username"]).first()
 
         self.assertEqual(register_1.status_code, 201)
         self.assertIsNotNone(user_expected)
-        register_2 = requests.post(REGISTER_URL, json = self.post_data2)
+        register_2 = client.post(REGISTER_URL, json = self.post_data2)
         self.assertEqual(register_2.status_code, 401)
-        self.assertEqual(register_2.json()["msg"],
+        self.assertEqual(register_2.get_json()["msg"],
                             "User already registered")
     
     def test_register_no_username(self):
@@ -67,9 +70,9 @@ class Test(unittest.TestCase):
         improper parameter of no username
         """
         corrupt_data = {"username": "", "password": "passwoor123"}
-        corrupt_post = requests.post(REGISTER_URL, json = corrupt_data)
+        corrupt_post = client.post(REGISTER_URL, json = corrupt_data)
         self.assertEqual(corrupt_post.status_code, 401)
-        self.assertEqual(corrupt_post.json()["msg"], "Username is required")
+        self.assertEqual(corrupt_post.get_json()["msg"], "Username is required")
 
     def test_register_no_password(self):
         """
@@ -77,46 +80,46 @@ class Test(unittest.TestCase):
         of an empy password
         """
         corrupt_data = {"username": "noPassword", "password": ""}
-        corrupt_post = requests.post(REGISTER_URL, json = corrupt_data)
+        corrupt_post = client.post(REGISTER_URL, json = corrupt_data)
         self.assertEqual(corrupt_post.status_code, 401)
-        self.assertEqual(corrupt_post.json()["msg"], "Password is required.")
+        self.assertEqual(corrupt_post.get_json()["msg"], "Password is required.")
     
     def test_login_no_user(self):
         """
         Tests login input on user not in the database
         """
-        post_data = requests.post(LOGIN_URL, json=self.post_data1)
+        post_data = client.post(LOGIN_URL, json=self.post_data1)
         self.assertEqual(post_data.status_code, 401)
-        self.assertEqual(post_data.json()["msg"], "Incorrect username.")
+        self.assertEqual(post_data.get_json()["msg"], "Incorrect username.")
 
     def test_login_valid(self):
         "Tests valid login user if user is registered"
-        register_1 = requests.post(REGISTER_URL, json = self.post_data1)
+        register_1 = client.post(REGISTER_URL, json = self.post_data1)
         self.assertEqual(register_1.status_code, 201)
     
-        post_data = requests.post(LOGIN_URL, json=self.post_data1)
+        post_data = client.post(LOGIN_URL, json=self.post_data1)
         self.assertEqual(post_data.status_code, 201)
-        self.assertEqual(post_data.json()["msg"], "Login Successful")
+        self.assertEqual(post_data.get_json()["msg"], "Login Successful")
 
     def test_user_not_registered(self):
         "Tests valid login user if other user is registered"
-        register_1 = requests.post(REGISTER_URL, json = self.post_data1)
+        register_1 = client.post(REGISTER_URL, json = self.post_data1)
         self.assertEqual(register_1.status_code, 201)
 
-        post_data = requests.post(LOGIN_URL, json=self.post_data2)
+        post_data = client.post(LOGIN_URL, json=self.post_data2)
         self.assertEqual(post_data.status_code, 401)
-        self.assertEqual(post_data.json()["msg"], "Incorrect username.")
+        self.assertEqual(post_data.get_json()["msg"], "Incorrect username.")
 
     def test_wrong_password(self):
         "Tests valid login user if user is registered"
-        register_1 = requests.post(REGISTER_URL, json = self.post_data1)
+        register_1 = client.post(REGISTER_URL, json = self.post_data1)
         self.assertEqual(register_1.status_code, 201)
 
         self.post_data1["password"] = "not_barry214"
 
-        post_data = requests.post(LOGIN_URL, json=self.post_data1)
+        post_data = client.post(LOGIN_URL, json=self.post_data1)
         self.assertEqual(post_data.status_code, 401)
-        self.assertEqual(post_data.json()["msg"], "Incorrect password.")
+        self.assertEqual(post_data.get_json()["msg"], "Incorrect password.")
 
     
         
